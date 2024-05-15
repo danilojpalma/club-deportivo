67736e23fd389550c36e2a1ad269a1b54a5aa5ad
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-05-15 15:02:45 -0400

    change some bootstrap styles

--- public/js/main.js
+++ public/js/main.js
@@ -13,9 +13,9 @@
           <td>${d.nombre}</td>
           <td>$${d.precio}</td>
           <td>
-            <button class="btn btn-warning" onclick='preEdit("${d.nombre}","${d.precio
+            <button class="btn btn-warning mx-4" onclick='preEdit("${d.nombre}","${d.precio
           }")' data-toggle="modal" data-target="#exampleModal">Editar</button>
-            <button class="btn btn-danger" onclick='eliminar("${d.nombre}")'>Eliminar</button>
+            <button class="btn btn-danger mx-4 my-lg-0 my-3" onclick='eliminar("${d.nombre}")'>Eliminar</button>
           </td>
         </tr>
         `)
